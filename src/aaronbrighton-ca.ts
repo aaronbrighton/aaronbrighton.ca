b06625011397b6d1acc4a6cabde5f61bae43a571
Aaron Brighton <aaron@aaronbrighton.ca>
2022-05-09 01:56:21 -0400

    Rendered this time

--- src/aaronbrighton-ca.ts
+++ src/aaronbrighton-ca.ts
@@ -58,7 +58,7 @@ export class AaronBrightonCaStack extends Stack {
         cacheControl: [
           s3deploy.CacheControl.setPublic(),
           s3deploy.CacheControl.mustRevalidate(),
-          s3deploy.CacheControl.maxAge(Duration.minutes(30)),
+          s3deploy.CacheControl.maxAge(Duration.minutes(1)),
         ],
         distribution: cloudfrontToS3Resource.cloudFrontWebDistribution,
       });
